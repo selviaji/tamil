e338d489fba15f72ed0bcae5ddc08ccd97ff5e50
selviaji <ajithngc@gmail.com>
2017-06-12 12:28:36 +0530

    Create waran.java

---
+++ waran.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Array6 {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int b=0;
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int k=0;k<n;k++){
+			a[k]=s.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if(a[i]<a[j]){
+				b=a[i];a[i]=a[j];
+		
+				a[j]=b;}}}
+			System.out.print("\t"+a[3]);
+			int k=s.nextInt();	
+				System.out.print("\t"+a[k-1]);
+	}
+}
